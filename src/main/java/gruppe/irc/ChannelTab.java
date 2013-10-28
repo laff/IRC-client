003d1583b46e696d7a263b92fb0875bcb3394137
Olaf Berge <olaf@highsoft.com>
2013-10-28 11:57:36 +0100

    New Classses
    After our meeting, this is how the structure should be like.

---
+++ src/main/java/gruppe/irc/ChannelTab.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gruppe.irc;
+
+/**
+ *
+ * @author HS Vikar
+ */
+public class ChannelTab {
+	
+}
