de3bdac7f82bceccf988943569edea910564ac7a
heloanders <abodung@hotmail.com>
2013-10-29 13:51:46 +0100

    Added look and feel
    In IRCClient.java

--- src/main/java/gruppe/irc/IRCClient.java
+++ src/main/java/gruppe/irc/IRCClient.java
@@ -5,9 +5,11 @@
 package gruppe.irc;
 
 import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 /**
  *
@@ -16,6 +18,23 @@ import javax.swing.UIManager;
 public class IRCClient {
 	
 	public static void main(String[] args) {
+		
+		//Sets look and feel to system default
+		try {
+			UIManager.setLookAndFeel( UIManager.getSystemLookAndFeelClassName() );
+		} catch (ClassNotFoundException f) {
+			System.out.println("Could not find system look and feel. Error: ");
+			f.printStackTrace();
+		} catch (InstantiationException f) {
+			System.out.println("Could not use system look and feel. Error: ");
+			f.printStackTrace();
+		} catch (IllegalAccessException f) {
+			System.out.println("Could not access system look and feel. Error: ");
+			f.printStackTrace();
+		} catch (UnsupportedLookAndFeelException f) {
+			System.out.println("Unsupported look and feel. Error: ");
+			f.printStackTrace();
+		}
 
 		//Schedule a job for the event dispatch thread:
 		//creating and showing this application's GUI.
